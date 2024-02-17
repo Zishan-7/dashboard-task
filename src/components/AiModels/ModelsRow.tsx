bc984d7ae050a801ce0e448298e02ec75de87760
Zishan Mohd <zishan2539@gmail.com>
2024-02-18 02:22:23 +0530

    Add avatar

--- src/components/AiModels/ModelsRow.tsx
+++ src/components/AiModels/ModelsRow.tsx
@@ -1,6 +1,7 @@
 import StarIcon from "@/assets/Icons/StarIcon";
 import Image, { StaticImageData } from "next/image";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 interface ModelsRowi {
   name: string;
@@ -23,7 +24,28 @@ const ModelsRow = ({ license, name, rating, version, logo }: ModelsRowi) => {
       <span className="w-[10%] text-center text-gray-200 font-light">
         {license}
       </span>
-      <span className="w-[20%] text-center">ssm</span>
+      <span className="relative w-[20%] flex justify-center items-center">
+        <Avatar className=" absolute left-8 w-7 h-7">
+          <AvatarImage src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
+          <AvatarFallback className="bg-black text-white text-[8px]">
+            CN
+          </AvatarFallback>
+        </Avatar>
+        <Avatar className=" absolute left-12 w-7 h-7">
+          <AvatarImage src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80" />
+
+          <AvatarFallback className="bg-black text-white text-[8px]">
+            CN
+          </AvatarFallback>
+        </Avatar>
+        <Avatar className=" absolute left-16 w-7 h-7">
+          <AvatarImage src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
+
+          <AvatarFallback className="bg-black text-white text-[8px]">
+            CN
+          </AvatarFallback>
+        </Avatar>
+      </span>
       <span className="w-[20%] flex justify-center items-center gap-x-1">
         {Array(rating)
           .fill("")
